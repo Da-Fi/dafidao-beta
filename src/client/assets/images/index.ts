97006b4dea468c6920304fc8fb8e45e613c826cb
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-07-01 16:54:49 -0400

    .

--- src/client/assets/images/index.ts
+++ src/client/assets/images/index.ts
@@ -1,6 +1,6 @@
 import AlchemyCertified from './alchemy-certified.svg';
-import YearnLogoFull from './qone-circle-64x64.svg';
-import YearnLogo from './quantum-one-logo.svg';
+import YearnLogoFull from './defiproto.svg';
+import YearnLogo from './defiproto2.svg';
 import YearnSplash from './yearn-splash.png';
 import MetamaskLogo from './metamask-logo.png';
 
